1377486f899d2dc75d6fedfec89da48830f5cc6a
yacine-boucetta <yacine.boucetta@laplateforme.io>
2021-12-02 10:49:02 +0100

    lien ok

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ session_start();
             <ul class="social">
                 <li><a href="https://twitter.com"
                         target=blank><img src="asset/image/twitter.png"></a></li>
-                <li><a href="https://www.instagram.com" target="_blank"><img class='insta'
+                <li><a href="https://www.instagram.com/explore/tags/pixelart/?hl=fr" target="_blank"><img class='insta'
                             src="asset/image/logo-instagram.png"></a></li>
-                <li><a href="https://fr-fr.facebook.com" target="_blank"><img
+                <li><a href="https://fr-fr.facebook.com/pixelartcolor/" target="_blank"><img
                             src="asset/image/facebook.png"></a>
                 </li>
-                <li><a href="https://github.com/yacine-boucetta/module-connexion" target="_blank"><img src="asset/image/téléchargement.png"></a></li>
+                <li><a href="https://github.com/yacine-boucetta/livre-or" target="_blank"><img src="asset/image/téléchargement.png"></a></li>
             </ul>
         </section>
     </main>
