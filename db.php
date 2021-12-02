fe9dfcdee5c723e4e35945fb93d17395f1e3ea47
yacine-boucetta <yacine.boucetta@laplateforme.io>
2021-12-02 10:44:18 +0100

    mise a jour plesk

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-$dbase= new PDO('mysql:host=localhost;dbname=livreor;charset=utf8','root','');
+$dbase= new PDO('mysql:host=localhost;dbname=yacine-boucetta_livre-or;charset=utf8','livre-or','projetpp2');
 [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
 }
 
